9a3907dfbf337cf67568b98732de5d139c616063
Hashem Poya <poya.kpu.2020@gmail.com>
2022-06-18 09:24:33 +0430

    [ADD/IMP] Add edit, delete feature in category section also improved slice and make an api requrest for fetching categories.

--- src/features/categories/CategoriesList.js
+++ src/features/categories/CategoriesList.js
@@ -1,19 +1,154 @@
-import { Card, CardBody, CardTitle, Table, Button } from "reactstrap";
-import { useSelector } from "react-redux";
-import { categoryRemoved } from "./categoriesSlice";
-import { useDispatch } from "react-redux";
+import {
+    Card,
+    CardBody,
+    CardTitle,
+    Table,
+    Button,
+    Modal,
+    ModalFooter,
+    ModalHeader,
+    ModalBody,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+} from "reactstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+
+import { categoryRemoved, categoryUpdated } from "./categoriesSlice";
+import { fetchCategories } from './categoriesSlice';
+
+import user1 from "../../assets/images/users/user1.jpg";
 
 const CategoriesList = () => {
 
+    const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+    const [catId, setCatId] = useState('');
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [available, setAvailable] = useState(false);
+
     const dispatch = useDispatch();
-    const categories = useSelector(state => state.categories);
+
+    const categories = useSelector(state => state.categories.categories);
+    const categoryStatus = useSelector(state => state.categories.status);
+
+    useEffect(() => {
+        if (categoryStatus === 'idle') {
+            dispatch(fetchCategories());
+        }
+    }, [dispatch, categoryStatus])
+
+    const toggleUpdate = () => setIsUpdateModalOpen(!isUpdateModalOpen);
+    const toggleDelete = () => setIsDeleteModalOpen(!isDeleteModalOpen);
+
+    const onNameChanged = e => setName(e.target.value);
+    const onDescriptionChanged = e => setDescription(e.target.value);
+    const onAvailableChanged = e => setAvailable(e.target.checked);
+
+    const deleteCategory = () => {
+        dispatch(categoryRemoved(catId));
+        setIsDeleteModalOpen(!isDeleteModalOpen);
+    }
+
+    const onUpdateButtonClicked = catId => {
+        toggleUpdate()
+        setCatId(catId);
+        const catItem = categories.find((item) => item.id === catId);
+        setName(catItem.name);
+        setDescription(catItem.description);
+        setAvailable(catItem.available)
+    }
+
+    const onUpdateFormSubmitted = e => {
+        e.preventDefault();
+        const newCategory = {
+            catId: catId,
+            avatar: user1,
+            name: name,
+            description: description,
+            available: available,
+        };
+        dispatch(categoryUpdated(newCategory));
+        toggleUpdate();
+
+    }
+
+    const deleteModal = (
+        <Modal isOpen={isDeleteModalOpen} toggle={toggleDelete}>
+            <ModalHeader
+                toggle={toggleDelete}>Warning</ModalHeader>
+            <ModalBody>
+                <h5>Are you sure you want to delete this category?</h5>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="danger" onClick={deleteCategory}>Delete</Button>
+                <Button onClick={toggleDelete}>
+                    Cancel
+                </Button>
+            </ModalFooter>
+        </Modal>
+    )
+
+    const updateModal = (
+        <Modal isOpen={isUpdateModalOpen} toggle={toggleUpdate}>
+            <Form onSubmit={onUpdateFormSubmitted}>
+                <ModalHeader
+                    toggle={toggleUpdate}>Update Category</ModalHeader>
+                <ModalBody>
+                    <FormGroup>
+                        <Label for="categoryName">Name</Label>
+                        <Input
+                            id="categoryName"
+                            name="text"
+                            placeholder="Category name"
+                            type="text"
+                            value={name}
+                            onChange={onNameChanged}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="categoryDescription">Description</Label>
+                        <Input
+                            id="categoryDescription"
+                            name="text"
+                            placeholder="Category description"
+                            type="text"
+                            value={description}
+                            onChange={onDescriptionChanged}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Input
+                            type="checkbox"
+                            value={available}
+                            checked={available}
+                            onChange={onAvailableChanged}
+                        />&nbsp;
+                        <Label check>Is category available?</Label>
+                    </FormGroup>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary">Update</Button>
+                    <Button onClick={toggleUpdate}>
+                        Cancel
+                    </Button>
+                </ModalFooter>
+            </Form>
+        </Modal>
+    );
 
     return (
         <div>
+            {updateModal}
+            {deleteModal}
             <Card>
                 <CardBody>
                     <CardTitle tag="h5">Categories List</CardTitle>
-                    <Table className="no-wrap mt-3 align-middle" responsive borderless>
+                    <Table className="no-wrap mt-3 align-middle" responsive borderless hover striped>
                         <thead>
                             <tr>
                                 <th>#</th>
@@ -53,12 +188,13 @@ const CategoriesList = () => {
                                         )}
                                     </td>
                                     <td>
-                                        <Button className="btn btn-sm" outline color="primary">
+                                        <Button className="btn btn-sm" outline color="primary" onClick={() => onUpdateButtonClicked(catData.id)}>
                                             Update
                                         </Button>
                                         &nbsp;
                                         <Button className="btn btn-sm" outline color="danger" onClick={() => {
-                                            dispatch(categoryRemoved(catData.id))
+                                            toggleDelete();
+                                            setCatId(catData.id);
                                         }}>
                                             Delete
                                         </Button>
--- src/features/categories/categoriesSlice.js
+++ src/features/categories/categoriesSlice.js
@@ -1,63 +1,66 @@
-import { nanoid, createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 import user1 from "../../assets/images/users/user1.jpg";
-import user2 from "../../assets/images/users/user2.jpg";
-import user3 from "../../assets/images/users/user3.jpg";
-import user4 from "../../assets/images/users/user4.jpg";
-import user5 from "../../assets/images/users/user5.jpg";
 
-const initialState = [
-    {
-        id: 1,
-        avatar: user1,
-        name: "Hanna Gover",
-        description: "some description about category",
-        status: "pending",
-        available: false,
-    },
-    {
-        id: 2,
-        avatar: user2,
-        name: "Hanna Gover",
-        description: "some description about category",
-        available: true,
-    },
-    {
-        id: 3,
-        avatar: user3,
-        name: "Hanna Gover",
-        description: "some description about category",
-        available: false,
-    },
-    {
-        id: 4,
-        avatar: user4,
-        name: "Hanna Gover",
-        description: "some description about category",
-        available: false,
-    },
-    {
-        id: 5,
-        avatar: user5,
-        name: "Hanna Gover",
-        description: "some description about category",
-        available: true,
-    },
-];
+const initialState = {
+    categories: [
+        {
+            id: 1,
+            avatar: user1,
+            name: "Hanna Gover",
+            description: "some description about category",
+            available: true,
+        },
+    ],
+    status: 'idle',
+    error: null,
+}
+
+export const fetchCategories = createAsyncThunk('categories/fetchCategories', async (_, thunkAPI) => {
+    try {
+        const response = await axios.get('http://localhost:8080/api/menu/category')
+        return response.data;    
+    }catch(err) {
+        return thunkAPI.rejectWithValue('something error happend.');
+    }
+
+});
 
 const categoriesSlice = createSlice({
     name: 'categories',
     initialState,
     reducers: {
         categoryAdded: (state, action) => {
-            state.push(action.payload);
+            state.categories.push(action.payload);
         },
         categoryRemoved: (state, action) => {
-            return state.filter((item) => item.id !== action.payload)
-        }
+            return state.categories.filter((item) => item.id !== action.payload)
+        },
+        categoryUpdated: (state, action) => {
+            const { catId, name, description, available } = action.payload;
+            const newState = state.categories.find(cat => cat.id === catId);
+            if (newState) {
+                newState.name = name;
+                newState.description = description;
+                newState.available = available;
+            }
+        },
+
+    },
+    extraReducers(builder) {
+        builder.addCase(fetchCategories.pending, (state) => {
+            state.status = 'loading'
+        }).addCase(fetchCategories.fulfilled, (state, action) => {
+            state.status = 'success'
+            state.categories = state.categories.concat(action.payload)
+        }).addCase(fetchCategories.rejected, (state, action) => {
+            state.status = 'failed'
+            state.error = action.error.message
+        });
     }
 });
 
 export default categoriesSlice.reducer;
 
-export const { categoryAdded, categoryRemoved } = categoriesSlice.actions; 
+export const { categoryAdded, categoryRemoved, categoryUpdated } = categoriesSlice.actions; 
