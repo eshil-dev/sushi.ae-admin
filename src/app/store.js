74bd43c55f4e079c8a2dcaf02a20d901cac399d3
Hashem Poya <poya.kpu.2020@gmail.com>
2022-06-19 11:11:12 +0430

    [RMD] menuSlice.js removed which no longer used.

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
-import menusReducer from '../features/menus/menusSlice';
 import { apiSlice } from '../features/api/apiSlice';
 
 export default configureStore({
     reducer: {
-        menus: menusReducer,
         [apiSlice.reducerPath]: apiSlice.reducer,
     },
     middleware: getDefaultMiddleware => getDefaultMiddleware()
