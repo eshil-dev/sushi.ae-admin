c70ead940c6798d682bfa29e108933ccad1bbf7c
Zaman Ehsani <zamanehsani@gmail.com>
2022-07-03 01:12:53 +0400

    updated the server.js file for __dirname

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 var app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
 
