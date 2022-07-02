3102ad88719abf994c1ee17622582385872f6f13
Zaman Ehsani <zamanehsani@gmail.com>
2022-07-03 00:49:22 +0400

    added server.js file for heroku deployment, and updated the node and browerlist

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 8080);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join('build', 'index.html'));
+    });
+  }
