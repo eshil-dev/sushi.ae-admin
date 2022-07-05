84808758a3ca2c07d3e1dd2e07ec83b09dde1014
Hashem Poya <poya.kpu.2020@gmail.com>
2022-07-05 12:06:29 +0430

    [IMP] Code indentation improved.

--- src/features/orders/CustomerCard.js
+++ src/features/orders/CustomerCard.js
@@ -6,19 +6,34 @@ const CustomerCard = ({ name, phone, address, location }) => {
     return (
         <div className='col '>
             <div className='border border-1 p-3 m-2'>
-                <div className='d-flex justify-content-between' style={{ marginBottom: "-20px" }}>
+                <div
+                    className='d-flex justify-content-between'
+                    style={{ marginBottom: "-20px" }}
+                >
                     <p className='fs-5 fw-bold'>{name}</p>
                     <p className='fs-5'>{phone}</p>
                 </div>
                 <hr />
                 <div className=''>
-                    <h5 className=' fw-bold'>Customer Address</h5>
+                    <h5 className='fw-bold'>Customer Address</h5>
                     <p className=''>{address}</p>
                 </div>
                 <div className=''>
-                    <ContentCopy className='mx-2 fs-2' onClick={() => console.log('oaky')} id='copy-icon' />
-                    <LocationOn className='mx-2 fs-2' onClick={() => console.log('oaky')} id='location-icon' />
-                    <Phone className='mx-2 fs-2' onClick={() => alert('This will call the customer')} id='phone-icon' />
+                    <ContentCopy
+                        className='mx-2 fs-2'
+                        onClick={() => console.log('oaky')}
+                        id='copy-icon'
+                    />
+                    <LocationOn
+                        className='mx-2 fs-2'
+                        onClick={() => console.log('oaky')}
+                        id='location-icon'
+                    />
+                    <Phone
+                        className='mx-2 fs-2'
+                        onClick={() => alert('This will call the customer')}
+                        id='phone-icon'
+                    />
                 </div>
             </div>
         </div>
